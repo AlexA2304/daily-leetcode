598d4a610f3a55967b7500a7ce5d7858d2644357
AlexA2300 <alar2300@colorado.edu>
2024-03-06 09:08:45 -0700

    Day 7: Majority Element

--- Problems/majority_element/me.py
+++ Problems/majority_element/me.py
@@ -1,3 +1,11 @@
+'''
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ 
+times. You may assume that the majority element always exists in 
+the array.
+'''
+
 class Solution:
     # boyer moore voting algorithm
     def majorityElement(self, nums: List[int]) -> int:
